3825560ab55b127108d4f5a9912ab088a635e50b
xushiwei <x@goplus.org>
2025-08-19 14:55:04 +0800

    _testgop: don't fmt

--- cl/_testgop/domaintext-json/out.go
+++ cl/_testgop/domaintext-json/out.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/goplus/xgo/tpl/encoding/json"
 )
 
--- cl/_testgop/rangeexpr/out.go
+++ cl/_testgop/rangeexpr/out.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/qiniu/x/gop"
+	"github.com/qiniu/x/xgo"
 )
 
 func main() {
 	fmt.Println(func() (_xgo_ret []int) {
-		for _xgo_it := gop.NewRange__0(0, 3, 1).Gop_Enum(); ; {
+		for _xgo_it := xgo.NewRange__0(0, 3, 1).Gop_Enum(); ; {
 			var _xgo_ok bool
 			x, _xgo_ok := _xgo_it.Next()
 			if !_xgo_ok {
