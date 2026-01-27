a5ae1d91d25fec2d801f730b05a341997f10c514
xushiwei <x@goplus.org>
2026-01-28 04:33:02 +0800

    gogen v1.20.8: Go does not fully implement chained call safety

--- cl/_testgop/map-field-access2/out.go
+++ cl/_testgop/map-field-access2/out.go
@@ -5,8 +5,12 @@ import "fmt"
 var v interface{}
 
 func main() {
-	c, ok := v.(map[string]any)["b"].(map[string]any)["c"].(int)
+	_autoGo_1, _ := v.(map[string]any)
+	_autoGo_2, _ := _autoGo_1["b"].(map[string]any)
+	c, ok := _autoGo_2["c"].(int)
 	fmt.Println(c, ok)
-	d, ok := v.(map[string]any)["b"].(map[string]any)["c"]
+	_autoGo_3, _ := v.(map[string]any)
+	_autoGo_4, _ := _autoGo_3["b"].(map[string]any)
+	d, ok := _autoGo_4["c"]
 	fmt.Println(d, ok)
 }
