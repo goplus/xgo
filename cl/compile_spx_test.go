d28af7ac4611447a9bbc507862e510031741a5e2
xushiwei <x@goplus.org>
2026-02-17 22:51:53 +0800

    cl: compileErrWrapExpr remove autoCall (#2638)

--- cl/compile_spx_test.go
+++ cl/compile_spx_test.go
@@ -119,7 +119,7 @@ Greem.t4spx:1:1: cannot use  (type *Greem) as type github.com/goplus/xgo/cl/inte
 		"/foo/Greem.t4spx": ``,
 	})
 
-	gopSpxErrorTestMap(t, `Game.t4gmx:1:9: assignment mismatch: 2 variables but this.BackdropName returns 1 values`, map[string][]string{
+	gopSpxErrorTestMap(t, `Game.t4gmx:1:9: cannot use backdropName (type string) as type error in assignment`, map[string][]string{
 		"/foo": {"Game.t4gmx"},
 	}, map[string]string{
 		"/foo/Game.t4gmx": `println backdropName!`,
