qiniu/qnlang

x/format: GopClass check project

x/format/gopclass_test.go
@@ -22,9 +22,9 @@ import (
 	"github.com/goplus/gop/x/format"
 )
 
-func testClass(t *testing.T, name string, pkg string, class string, entry string, src, expect string) {
+func testClass(t *testing.T, name string, pkg string, class string, proj bool, src, expect string) {
 	t.Run(name, func(t *testing.T) {
-		result, err := format.GopClassSource([]byte(src), pkg, class, entry, name)
+		result, err := format.GopClassSource([]byte(src), pkg, class, proj, name)
 		if err != nil {
 			t.Fatal("format.GopClassSource failed:", err)
 		}
@@ -35,7 +35,7 @@ func testClass(t *testing.T, name string, pkg string, class string, entry string
 }
 
 func TestClassSpx(t *testing.T) {
-	testClass(t, "spx class", "github.com/goplus/spx", "Calf", "Main", `package main
+	testClass(t, "spx class", "github.com/goplus/spx", "Calf", false, `package main
 
 import (
 	"github.com/goplus/spx"
@@ -58,6 +58,35 @@ func (this *Calf) Dump() {
 func (this *Calf) Main() {
 	this.OnStart(func() {
 		this.Say("Hello Go+")
+	})
+//line Calf.spx:38:1
+	this.OnMsg__1("tap", func() {
+//line Calf.spx:39:1
+		for calfPlay {
+			spx.Sched()
+//line Calf.spx:40:1
+			for !(this.KeyPressed(spx.KeySpace) || this.MousePressed()) {
+				spx.Sched()
+//line Calf.spx:41:1
+				this.Wait(0.01)
+			}
+//line Calf.spx:43:1
+			calfGravity = 0.8
+//line Calf.spx:44:1
+			for
+//line Calf.spx:44:1
+			i := 0; i < 10;
+//line Calf.spx:44:1
+			i++ {
+				spx.Sched()
+//line Calf.spx:45:1
+				this.ChangeYpos(3.5)
+//line Calf.spx:46:1
+				this.Wait(0.03)
+			}
+//line Calf.spx:48:1
+			this.Wait(0.03)
+		}
 	})
 }
 func (this *Calf) Classfname() string {
@@ -83,6 +112,88 @@ func Dump() {
 onStart func() {
 	say "Hello Go+"
 }
-`)
 
+onMsg__1 "tap", func() {
+
+	for calfPlay {
+
+		for !(keyPressed(KeySpace) || mousePressed()) {
+
+			wait 0.01
+		}
+
+		calfGravity = 0.8
+
+		for i := 0; i < 10; i++ {
+
+			changeYpos 3.5
+
+			wait 0.03
+		}
+
+		wait 0.03
+	}
+}
+`)
+}
+
+func TestClassProj(t *testing.T) {
+	testClass(t, "spx project", "github.com/goplus/spx", "Game", true, `package main
+
+import "github.com/goplus/spx"
+import "log"
+
+type Game struct {
+	spx.Game
+	MyAircraft MyAircraft
+	Bullet     Bullet
+}
+
+var calfPlay = false
+var calfDie = false
+var calfGravity = 0.0
+//line main.spx:30:1
+func (this *Game) reset() {
+//line main.spx:31:1
+	this.userScore = 0
+//line main.spx:32:1
+	calfPlay = false
+//line main.spx:33:1
+	calfDie = false
+//line main.spx:34:1
+	calfGravity = 0.0
+}
+func (this *Game) MainEntry() {
+	log.Println("MainEntry")
+}
+func (this *Game) Main() {
+	spx.Gopt_Game_Main(this, new(Bullet), new(MyAircraft))
+}
+func main() {
+	new(Game).Main()
+}
+`, `import "log"
+
+var (
+	MyAircraft MyAircraft
+	Bullet     Bullet
+)
+
+var calfPlay = false
+var calfDie = false
+var calfGravity = 0.0
+
+func reset() {
+
+	userScore = 0
+
+	calfPlay = false
+
+	calfDie = false
+
+	calfGravity = 0.0
+}
+
+log.println "MainEntry"
+`)
 }

x/format/gopstyle.go
@@ -69,15 +69,15 @@ func Gopstyle(file *ast.File) {
 	formatFile(file)
 }
 
-func GopClassSource(src []byte, pkg string, class string, entry string, filename ...string) (ret []byte, err error) {
+func GopClassSource(src []byte, pkg string, class string, proj bool, filename ...string) (ret []byte, err error) {
 	var fname string
 	if filename != nil {
 		fname = filename[0]
 	}
 	fset := token.NewFileSet()
 	var f *ast.File
-	if f, err = parser.ParseFile(fset, fname, src, parser.ParseComments); err == nil {
-		GopClass(f, pkg, class, entry)
+	if f, err = parser.ParseFile(fset, fname, src, parser.AllErrors); err == nil {
+		GopClass(f, pkg, class, proj)
 		var buf bytes.Buffer
 		if err = format.Node(&buf, fset, f); err == nil {
 			ret = buf.Bytes()
@@ -87,8 +87,8 @@ func GopClassSource(src []byte, pkg string, class string, entry string, filename
 }
 
 // GopClass format ast.File to Go+ class
-func GopClass(file *ast.File, pkg string, class string, entry string) {
-	formatClass(file, pkg, class, entry)
+func GopClass(file *ast.File, pkg string, class string, proj bool) {
+	formatClass(file, pkg, class, proj)
 }
 
 func findFuncDecl(decls []ast.Decl, name string) (int, *ast.FuncDecl) {
@@ -235,7 +235,7 @@ func formatFile(file *ast.File) {
 	}
 }
 
-func formatClass(file *ast.File, pkg string, class string, entry string) {
+func formatClass(file *ast.File, pkg string, class string, proj bool) {
 	var funcs []*ast.FuncDecl
 	ctx := &formatCtx{
 		imports:   make(map[string]*importCtx),
@@ -257,13 +257,24 @@ func formatClass(file *ast.File, pkg string, class string, entry string) {
 			if isClassFunc(v, class) {
 				v.IsClass = true
 				switch v.Name.Name {
-				case entry:
-					fnEntry = v
-					file.ShadowEntry = v
-					continue
+				case "MainEntry":
+					if proj {
+						fnEntry = v
+						file.ShadowEntry = v
+						continue
+					}
+				case "Main":
+					if !proj {
+						fnEntry = v
+						file.ShadowEntry = v
+						continue
+					}
+					v.Shadow = true
 				case "Classfname":
 					v.Shadow = true
 				}
+			} else if v.Name.Name == "main" && proj {
+				v.Shadow = true
 			}
 		case *ast.GenDecl:
 			switch v.Tok {
@@ -309,7 +320,7 @@ func formatClass(file *ast.File, pkg string, class string, entry string) {
 					var pkgPath = toString(spec.Path)
 					var name string
 					if spec.Name == nil {
-						name = path.Base(pkgPath) // TODO: open pkgPath to get pkgName
+						name = path.Base(pkgPath)
 					} else {
 						name = spec.Name.Name
 						if name == "." || name == "_" {

goplus/xgo

x/format/gopclass_test.go
@@ -19,14 +19,14 @@ package format_test
 import (
 	"testing"
 
-	"github.com/goplus/gop/x/format"
+	"github.com/goplus/xgo/x/format"
 )
 
-func testClass(t *testing.T, name string, pkg string, class string, entry string, src, expect string) {
+func testClass(t *testing.T, name string, pkg string, class string, proj bool, src, expect string) {
 	t.Run(name, func(t *testing.T) {
-		result, err := format.GopClassSource([]byte(src), pkg, class, entry, name)
+		result, err := format.XGoClassSource([]byte(src), pkg, class, proj, name)
 		if err != nil {
-			t.Fatal("format.GopClassSource failed:", err)
+			t.Fatal("format.XGoClassSource failed:", err)
 		}
 		if ret := string(result); ret != expect {
 			t.Fatalf("%s => Expect:\n%s\n=> Got:\n%s\n", name, expect, ret)
@@ -35,7 +35,7 @@ func testClass(t *testing.T, name string, pkg string, class string, entry string
 }
 
 func TestClassSpx(t *testing.T) {
-	testClass(t, "spx class", "github.com/goplus/spx", "Calf", "Main", `package main
+	testClass(t, "spx class", "github.com/goplus/spx", "Calf", false, `package main
 
 import (
 	"github.com/goplus/spx"
@@ -58,6 +58,35 @@ func (this *Calf) Dump() {
 func (this *Calf) Main() {
 	this.OnStart(func() {
 		this.Say("Hello Go+")
+	})
+//line Calf.spx:38:1
+	this.OnMsg__1("tap", func() {
+//line Calf.spx:39:1
+		for calfPlay {
+			spx.Sched()
+//line Calf.spx:40:1
+			for !(this.KeyPressed(spx.KeySpace) || this.MousePressed()) {
+				spx.Sched()
+//line Calf.spx:41:1
+				this.Wait(0.01)
+			}
+//line Calf.spx:43:1
+			calfGravity = 0.8
+//line Calf.spx:44:1
+			for
+//line Calf.spx:44:1
+			i := 0; i < 10;
+//line Calf.spx:44:1
+			i++ {
+				spx.Sched()
+//line Calf.spx:45:1
+				this.ChangeYpos(3.5)
+//line Calf.spx:46:1
+				this.Wait(0.03)
+			}
+//line Calf.spx:48:1
+			this.Wait(0.03)
+		}
 	})
 }
 func (this *Calf) Classfname() string {
@@ -83,6 +112,88 @@ func Dump() {
 onStart func() {
 	say "Hello Go+"
 }
-`)
 
+onMsg__1 "tap", func() {
+
+	for calfPlay {
+
+		for !(keyPressed(KeySpace) || mousePressed()) {
+
+			wait 0.01
+		}
+
+		calfGravity = 0.8
+
+		for i := 0; i < 10; i++ {
+
+			changeYpos 3.5
+
+			wait 0.03
+		}
+
+		wait 0.03
+	}
+}
+`)
+}
+
+func TestClassProj(t *testing.T) {
+	testClass(t, "spx project", "github.com/goplus/spx", "Game", true, `package main
+
+import "github.com/goplus/spx"
+import "log"
+
+type Game struct {
+	spx.Game
+	MyAircraft MyAircraft
+	Bullet     Bullet
+}
+
+var calfPlay = false
+var calfDie = false
+var calfGravity = 0.0
+//line main.spx:30:1
+func (this *Game) reset() {
+//line main.spx:31:1
+	this.userScore = 0
+//line main.spx:32:1
+	calfPlay = false
+//line main.spx:33:1
+	calfDie = false
+//line main.spx:34:1
+	calfGravity = 0.0
+}
+func (this *Game) MainEntry() {
+	log.Println("MainEntry")
+}
+func (this *Game) Main() {
+	spx.Gopt_Game_Main(this, new(Bullet), new(MyAircraft))
+}
+func main() {
+	new(Game).Main()
+}
+`, `import "log"
+
+var (
+	MyAircraft MyAircraft
+	Bullet     Bullet
+)
+
+var calfPlay = false
+var calfDie = false
+var calfGravity = 0.0
+
+func reset() {
+
+	userScore = 0
+
+	calfPlay = false
+
+	calfDie = false
+
+	calfGravity = 0.0
+}
+
+log.println "MainEntry"
+`)
 }

x/format/gopstyle.go
@@ -83,7 +83,7 @@ func Gopstyle(file *ast.File) {
 	XGoStyle(file)
 }
 
-func XGoClassSource(src []byte, pkg string, class string, entry string, filename ...string) (ret []byte, err error) {
+func XGoClassSource(src []byte, pkg string, class string, prog bool, filename ...string) (ret []byte, err error) {
 	var fname string
 	if filename != nil {
 		fname = filename[0]
@@ -91,7 +91,7 @@ func XGoClassSource(src []byte, pkg string, class string, entry string, filename
 	fset := token.NewFileSet()
 	var f *ast.File
 	if f, err = parser.ParseFile(fset, fname, src, parser.ParseComments); err == nil {
-		XGoClass(f, pkg, class, entry)
+		XGoClass(f, pkg, class, prog)
 		var buf bytes.Buffer
 		if err = format.Node(&buf, fset, f); err == nil {
 			ret = buf.Bytes()
@@ -100,9 +100,9 @@ func XGoClassSource(src []byte, pkg string, class string, entry string, filename
 	return
 }
 
-// GopClass format ast.File to Go+ class
-func XGoClass(file *ast.File, pkg string, class string, entry string) {
-	formatClass(file, pkg, class, entry)
+// XGoClass format ast.File to Go+ class
+func XGoClass(file *ast.File, pkg string, class string, prog bool) {
+	formatClass(file, pkg, class, prog)
 }
 
 func findFuncDecl(decls []ast.Decl, name string) (int, *ast.FuncDecl) {
@@ -249,7 +249,7 @@ func formatFile(file *ast.File) {
 	}
 }
 
-func formatClass(file *ast.File, pkg string, class string, entry string) {
+func formatClass(file *ast.File, pkg string, class string, proj bool) {
 	var funcs []*ast.FuncDecl
 	ctx := &formatCtx{
 		imports:   make(map[string]*importCtx),
@@ -271,13 +271,24 @@ func formatClass(file *ast.File, pkg string, class string, entry string) {
 			if isClassFunc(v, class) {
 				v.IsClass = true
 				switch v.Name.Name {
-				case entry:
-					fnEntry = v
-					file.ShadowEntry = v
-					continue
+				case "MainEntry":
+					if proj {
+						fnEntry = v
+						file.ShadowEntry = v
+						continue
+					}
+				case "Main":
+					if !proj {
+						fnEntry = v
+						file.ShadowEntry = v
+						continue
+					}
+					v.Shadow = true
 				case "Classfname":
 					v.Shadow = true
 				}
+			} else if v.Name.Name == "main" && proj {
+				v.Shadow = true
 			}
 		case *ast.GenDecl:
 			switch v.Tok {
@@ -323,7 +334,7 @@ func formatClass(file *ast.File, pkg string, class string, entry string) {
 					var pkgPath = toString(spec.Path)
 					var name string
 					if spec.Name == nil {
-						name = path.Base(pkgPath) // TODO: open pkgPath to get pkgName
+						name = path.Base(pkgPath)
 					} else {
 						name = spec.Name.Name
 						if name == "." || name == "_" {