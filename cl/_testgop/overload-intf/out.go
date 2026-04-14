ac37dfa81fadda7cbf3c9685807a7eb69b045aed
xushiwei <x@goplus.org>
2026-04-14 19:39:42 +0800

    cl/_testgop/overload-intf: turnTo, quote

--- cl/_testgop/overload-intf/out.go
+++ cl/_testgop/overload-intf/out.go
@@ -4,4 +4,7 @@ import "github.com/goplus/xgo/cl/internal/overload/intf"
 
 func f(sp intf.Sprite) {
 	sp.GlideToXYpos(10, 20, 0.01)
+	sp.TurnToDir(90, nil)
+	sp.TurnToTarget("Kai", nil)
+	sp.QuoteMsg("Hi", 0)
 }
