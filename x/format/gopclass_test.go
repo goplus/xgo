qiniu/qnlang

x/format: ClassConfig support gopt/overload

x/format/gopclass_test.go
@@ -22,20 +22,24 @@ import (
 	"github.com/goplus/gop/x/format"
 )
 
-func testClass(t *testing.T, name string, pkg string, class string, proj bool, src, expect string) {
+func testClass(t *testing.T, name string, cfg *format.ClassConfig, src, expect string) {
 	t.Run(name, func(t *testing.T) {
-		result, err := format.GopClassSource([]byte(src), pkg, class, proj, name)
+		result, err := format.GopClassSource([]byte(src), cfg, name)
 		if err != nil {
 			t.Fatal("format.GopClassSource failed:", err)
 		}
 		if ret := string(result); ret != expect {
-			t.Fatalf("%s => Expect:\n%s\n=> Got:\n%s\n", name, expect, ret)
+			t.Fatalf("%s => Expect:\n%s\n=> Got:\n%s\n== end", name, expect, ret)
 		}
 	})
 }
 
 func TestClassSpx(t *testing.T) {
-	testClass(t, "spx class", "github.com/goplus/spx", "Calf", false, `package main
+	testClass(t, "spx class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/spx",
+		ClassName: "Calf",
+		Overload:  map[string]string{"OnMsg__1": "OnMsg"},
+	}, `package main
 
 import (
 	"github.com/goplus/spx"
@@ -113,7 +117,7 @@ onStart func() {
 	say "Hello Go+"
 }
 
-onMsg__1 "tap", func() {
+onMsg "tap", func() {
 
 	for calfPlay {
 
@@ -138,7 +142,11 @@ onMsg__1 "tap", func() {
 }
 
 func TestClassProj(t *testing.T) {
-	testClass(t, "spx project", "github.com/goplus/spx", "Game", true, `package main
+	testClass(t, "spx project", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/spx",
+		ClassName: "Game",
+		Project:   true,
+	}, `package main
 
 import "github.com/goplus/spx"
 import "log"
@@ -152,15 +160,10 @@ type Game struct {
 var calfPlay = false
 var calfDie = false
 var calfGravity = 0.0
-//line main.spx:30:1
 func (this *Game) reset() {
-//line main.spx:31:1
 	this.userScore = 0
-//line main.spx:32:1
 	calfPlay = false
-//line main.spx:33:1
 	calfDie = false
-//line main.spx:34:1
 	calfGravity = 0.0
 }
 func (this *Game) MainEntry() {
@@ -184,13 +187,9 @@ var calfDie = false
 var calfGravity = 0.0
 
 func reset() {
-
 	userScore = 0
-
 	calfPlay = false
-
 	calfDie = false
-
 	calfGravity = 0.0
 }
 
@@ -199,7 +198,10 @@ log.println "MainEntry"
 }
 
 func TestClassGox(t *testing.T) {
-	testClass(t, "gox class", "", "Rect", false, `package main
+	testClass(t, "gox class", &format.ClassConfig{
+		ClassName: "Rect",
+		Comments:  true,
+	}, `package main
 
 type BaseClass struct {
 	x int
@@ -214,6 +216,7 @@ type Rect struct {
 	*AggClass
 }
 
+// Area is call rect area
 func (this *Rect) Area() float64 {
 	return this.Width * this.Height
 }
@@ -232,8 +235,107 @@ type BaseClass struct {
 type AggClass struct {
 }
 
+// Area is call rect area
 func Area() float64 {
 	return Width * Height
 }
+`)
+}
+
+func TestClassGopt(t *testing.T) {
+	testClass(t, "test class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/gop/cl/internal/spx",
+		ClassName: "Game",
+		Project:   true,
+		Gopt: map[string]string{
+			"Gopt_Sprite_Clone__0": "Clone",
+			"Gopt_Sprite_Clone__1": "Clone",
+		},
+		Overload: map[string]string{"Broadcast__0": "Broadcast"},
+	}, `package main
+
+import "github.com/goplus/gop/cl/internal/spx"
+
+type Game struct {
+	*spx.MyGame
+	Kai Kai
+}
+func (this *Game) onInit() {
+	spx.Gopt_Sprite_Clone__0(this.Kai)
+	this.Broadcast__0("msg1")
+}
+func (this *Game) MainEntry() {
+}
+func (this *Game) Main() {
+	spx.Gopt_MyGame_Main(this)
+}
+func main() {
+	new(Game).Main()
+}
+`, `var Kai Kai
+
+func onInit() {
+	Kai.clone
+	broadcast "msg1"
+}
+
+
+`)
+	testClass(t, "test class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/gop/cl/internal/spx",
+		ClassName: "Kai",
+		Gopt: map[string]string{
+			"Gopt_Sprite_Clone__0": "Clone",
+			"Gopt_Sprite_Clone__1": "Clone",
+		},
+		Overload: map[string]string{"Broadcast__0": "Broadcast"},
+	}, `package main
+
+import "github.com/goplus/gop/cl/internal/spx"
+
+type info struct {
+	x int
+	y int
+}
+
+type Kai struct {
+	spx.Sprite
+	*Game
+	a int
+}
+
+func (this *Kai) onInit() {
+	this.a = 1
+	spx.Gopt_Sprite_Clone__0(this)
+	spx.Gopt_Sprite_Clone__1(this, info{1, 2})
+	spx.Gopt_Sprite_Clone__1(this, &info{1, 2})
+}
+func (this *Kai) onCloned() {
+	this.Say("Hi")
+}
+func (this *Kai) Classfname() string {
+	return "Kai"
+}
+func (this *Kai) Main() {
+}
+`, `var a int
+
+type info struct {
+	x int
+	y int
+}
+
+func onInit() {
+	a = 1
+	clone
+	clone info{1, 2}
+	clone &info{1, 2}
+}
+
+func onCloned() {
+	say "Hi"
+}
+
+
 `)
 }

x/format/gopstyle.go
@@ -69,18 +69,29 @@ func Gopstyle(file *ast.File) {
 	formatFile(file)
 }
 
+type ClassConfig struct {
+	PkgPath   string            // Go+ class project pkgpath, empty if normal .gox class. (optional)
+	ClassName string            // project or class name.
+	Project   bool              // true means ClassName is project.
+	Comments  bool              // true means parse comments.
+	Gopt      map[string]string // Gopt_ function name mapping. (optional)
+	Overload  map[string]string // Overload function name mapping. (optional)
+}
+
 // GopClassSource format Go+ source to Go+ class source.
-// pkg set class project pkg path, emtpy if normal .gox class.
-// class set the class name. proj indication project or class.
-func GopClassSource(src []byte, pkg string, class string, proj bool, filename ...string) (ret []byte, err error) {
+func GopClassSource(src []byte, cfg *ClassConfig, filename ...string) (ret []byte, err error) {
 	var fname string
 	if filename != nil {
 		fname = filename[0]
 	}
 	fset := token.NewFileSet()
+	mode := parser.AllErrors
+	if cfg.Comments {
+		mode |= parser.ParseComments
+	}
 	var f *ast.File
-	if f, err = parser.ParseFile(fset, fname, src, parser.AllErrors); err == nil {
-		GopClass(f, pkg, class, proj)
+	if f, err = parser.ParseFile(fset, fname, src, mode); err == nil {
+		GopClass(f, cfg)
 		var buf bytes.Buffer
 		if err = format.Node(&buf, fset, f); err == nil {
 			ret = buf.Bytes()
@@ -90,8 +101,8 @@ func GopClassSource(src []byte, pkg string, class string, proj bool, filename ..
 }
 
 // GopClass format ast.File to Go+ class
-func GopClass(file *ast.File, pkg string, class string, proj bool) {
-	formatClass(file, pkg, class, proj)
+func GopClass(file *ast.File, cfg *ClassConfig) {
+	formatClass(file, cfg)
 }
 
 func findFuncDecl(decls []ast.Decl, name string) (int, *ast.FuncDecl) {
@@ -167,12 +178,11 @@ type importCtx struct {
 }
 
 type formatCtx struct {
-	imports   map[string]*importCtx
-	scope     *types.Scope
-	classMode bool   //class mode
-	classPkg  string //class pkg name
-	className string //this class
-	funcRecv  string //this class func recv
+	imports  map[string]*importCtx
+	scope    *types.Scope
+	classCfg *ClassConfig
+	classPkg string //this class pkg name
+	funcRecv string //this class func recv
 }
 
 func (ctx *formatCtx) insert(name string) {
@@ -238,14 +248,13 @@ func formatFile(file *ast.File) {
 	}
 }
 
-func formatClass(file *ast.File, pkg string, class string, proj bool) {
+func formatClass(file *ast.File, cfg *ClassConfig) {
 	var funcs []*ast.FuncDecl
 	ctx := &formatCtx{
-		imports:   make(map[string]*importCtx),
-		scope:     types.NewScope(nil, token.NoPos, token.NoPos, ""),
-		classMode: true,
-		classPkg:  path.Base(pkg),
-		className: class,
+		imports:  make(map[string]*importCtx),
+		scope:    types.NewScope(nil, token.NoPos, token.NoPos, ""),
+		classCfg: cfg,
+		classPkg: path.Base(cfg.PkgPath),
 	}
 	if file.Name.Name == "main" {
 		file.NoPkgDecl = true
@@ -257,17 +266,17 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 	for _, decl := range file.Decls {
 		switch v := decl.(type) {
 		case *ast.FuncDecl:
-			if isClassFunc(v, class) {
+			if isClassFunc(v, cfg.ClassName) {
 				v.IsClass = true
 				switch v.Name.Name {
 				case "MainEntry":
-					if proj {
+					if cfg.Project {
 						fnEntry = v
 						file.ShadowEntry = v
 						continue
 					}
 				case "Main":
-					if !proj {
+					if !cfg.Project {
 						fnEntry = v
 						file.ShadowEntry = v
 						continue
@@ -276,7 +285,7 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 				case "Classfname":
 					v.Shadow = true
 				}
-			} else if v.Name.Name == "main" && proj {
+			} else if v.Name.Name == "main" && cfg.Project {
 				v.Shadow = true
 			}
 		case *ast.GenDecl:
@@ -285,10 +294,10 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 				imports = append(imports, v)
 				continue
 			case token.TYPE:
-				if spec, ok := v.Specs[0].(*ast.TypeSpec); ok && spec.Name.Name == class {
+				if spec, ok := v.Specs[0].(*ast.TypeSpec); ok && spec.Name.Name == cfg.ClassName {
 					if st, ok := spec.Type.(*ast.StructType); ok {
 						for _, fs := range st.Fields.List {
-							if len(fs.Names) == 0 && pkg != "" {
+							if len(fs.Names) == 0 && cfg.PkgPath != "" {
 								continue
 							}
 							varSpecs = append(varSpecs, &ast.ValueSpec{Names: fs.Names, Type: fs.Type})
@@ -392,7 +401,7 @@ func funcRecv(v *ast.FuncDecl) *ast.Ident {
 }
 
 func formatFuncDecl(ctx *formatCtx, v *ast.FuncDecl) {
-	if ctx.classMode && isClassFunc(v, ctx.className) {
+	if ctx.classCfg != nil && isClassFunc(v, ctx.classCfg.ClassName) {
 		v.IsClass = true
 		if recv := funcRecv(v); recv != nil {
 			ctx.funcRecv = recv.Name

goplus/xgo

x/format/gopclass_test.go
@@ -22,20 +22,24 @@ import (
 	"github.com/goplus/xgo/x/format"
 )
 
-func testClass(t *testing.T, name string, pkg string, class string, proj bool, src, expect string) {
+func testClass(t *testing.T, name string, cfg *format.ClassConfig, src, expect string) {
 	t.Run(name, func(t *testing.T) {
-		result, err := format.XGoClassSource([]byte(src), pkg, class, proj, name)
+		result, err := format.XGoClassSource([]byte(src), cfg, name)
 		if err != nil {
 			t.Fatal("format.XGoClassSource failed:", err)
 		}
 		if ret := string(result); ret != expect {
-			t.Fatalf("%s => Expect:\n%s\n=> Got:\n%s\n", name, expect, ret)
+			t.Fatalf("%s => Expect:\n%s\n=> Got:\n%s\n== end", name, expect, ret)
 		}
 	})
 }
 
 func TestClassSpx(t *testing.T) {
-	testClass(t, "spx class", "github.com/goplus/spx", "Calf", false, `package main
+	testClass(t, "spx class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/spx",
+		ClassName: "Calf",
+		Overload:  map[string]string{"OnMsg__1": "OnMsg"},
+	}, `package main
 
 import (
 	"github.com/goplus/spx"
@@ -109,11 +113,11 @@ func Dump() {
 	log.println info
 }
 
-onStart func() {
+onStart => {
 	say "Hello Go+"
 }
 
-onMsg__1 "tap", func() {
+onMsg "tap", => {
 
 	for calfPlay {
 
@@ -138,7 +142,11 @@ onMsg__1 "tap", func() {
 }
 
 func TestClassProj(t *testing.T) {
-	testClass(t, "spx project", "github.com/goplus/spx", "Game", true, `package main
+	testClass(t, "spx project", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/spx",
+		ClassName: "Game",
+		Project:   true,
+	}, `package main
 
 import "github.com/goplus/spx"
 import "log"
@@ -152,15 +160,10 @@ type Game struct {
 var calfPlay = false
 var calfDie = false
 var calfGravity = 0.0
-//line main.spx:30:1
 func (this *Game) reset() {
-//line main.spx:31:1
 	this.userScore = 0
-//line main.spx:32:1
 	calfPlay = false
-//line main.spx:33:1
 	calfDie = false
-//line main.spx:34:1
 	calfGravity = 0.0
 }
 func (this *Game) MainEntry() {
@@ -184,13 +187,9 @@ var calfDie = false
 var calfGravity = 0.0
 
 func reset() {
-
 	userScore = 0
-
 	calfPlay = false
-
 	calfDie = false
-
 	calfGravity = 0.0
 }
 
@@ -199,7 +198,10 @@ log.println "MainEntry"
 }
 
 func TestClassGox(t *testing.T) {
-	testClass(t, "gox class", "", "Rect", false, `package main
+	testClass(t, "gox class", &format.ClassConfig{
+		ClassName: "Rect",
+		Comments:  true,
+	}, `package main
 
 type BaseClass struct {
 	x int
@@ -214,6 +216,7 @@ type Rect struct {
 	*AggClass
 }
 
+// Area is call rect area
 func (this *Rect) Area() float64 {
 	return this.Width * this.Height
 }
@@ -232,8 +235,107 @@ type BaseClass struct {
 type AggClass struct {
 }
 
+// Area is call rect area
 func Area() float64 {
 	return Width * Height
 }
+`)
+}
+
+func TestClassGopt(t *testing.T) {
+	testClass(t, "test class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/gop/cl/internal/spx",
+		ClassName: "Game",
+		Project:   true,
+		Gopt: map[string]string{
+			"Gopt_Sprite_Clone__0": "Clone",
+			"Gopt_Sprite_Clone__1": "Clone",
+		},
+		Overload: map[string]string{"Broadcast__0": "Broadcast"},
+	}, `package main
+
+import "github.com/goplus/gop/cl/internal/spx"
+
+type Game struct {
+	*spx.MyGame
+	Kai Kai
+}
+func (this *Game) onInit() {
+	spx.Gopt_Sprite_Clone__0(this.Kai)
+	this.Broadcast__0("msg1")
+}
+func (this *Game) MainEntry() {
+}
+func (this *Game) Main() {
+	spx.Gopt_MyGame_Main(this)
+}
+func main() {
+	new(Game).Main()
+}
+`, `var Kai Kai
+
+func onInit() {
+	Kai.clone
+	broadcast "msg1"
+}
+
+
+`)
+	testClass(t, "test class", &format.ClassConfig{
+		PkgPath:   "github.com/goplus/gop/cl/internal/spx",
+		ClassName: "Kai",
+		Gopt: map[string]string{
+			"Gopt_Sprite_Clone__0": "Clone",
+			"Gopt_Sprite_Clone__1": "Clone",
+		},
+		Overload: map[string]string{"Broadcast__0": "Broadcast"},
+	}, `package main
+
+import "github.com/goplus/gop/cl/internal/spx"
+
+type info struct {
+	x int
+	y int
+}
+
+type Kai struct {
+	spx.Sprite
+	*Game
+	a int
+}
+
+func (this *Kai) onInit() {
+	this.a = 1
+	spx.Gopt_Sprite_Clone__0(this)
+	spx.Gopt_Sprite_Clone__1(this, info{1, 2})
+	spx.Gopt_Sprite_Clone__1(this, &info{1, 2})
+}
+func (this *Kai) onCloned() {
+	this.Say("Hi")
+}
+func (this *Kai) Classfname() string {
+	return "Kai"
+}
+func (this *Kai) Main() {
+}
+`, `var a int
+
+type info struct {
+	x int
+	y int
+}
+
+func onInit() {
+	a = 1
+	clone
+	clone info{1, 2}
+	clone &info{1, 2}
+}
+
+func onCloned() {
+	say "Hi"
+}
+
+
 `)
 }

x/format/gopstyle.go
@@ -83,15 +83,28 @@ func Gopstyle(file *ast.File) {
 	XGoStyle(file)
 }
 
-func XGoClassSource(src []byte, pkg string, class string, proj bool, filename ...string) (ret []byte, err error) {
+type ClassConfig struct {
+	PkgPath   string            // Go+ class project pkgpath, empty if normal .gox class. (optional)
+	ClassName string            // project or class name.
+	Project   bool              // true means ClassName is project.
+	Comments  bool              // true means parse comments.
+	Gopt      map[string]string // Gopt_ function name mapping. (optional)
+	Overload  map[string]string // Overload function name mapping. (optional)
+}
+
+func XGoClassSource(src []byte, cfg *ClassConfig, filename ...string) (ret []byte, err error) {
 	var fname string
 	if filename != nil {
 		fname = filename[0]
 	}
 	fset := token.NewFileSet()
+	mode := parser.AllErrors
+	if cfg.Comments {
+		mode |= parser.ParseComments
+	}
 	var f *ast.File
-	if f, err = parser.ParseFile(fset, fname, src, parser.ParseComments); err == nil {
-		XGoClass(f, pkg, class, proj)
+	if f, err = parser.ParseFile(fset, fname, src, mode); err == nil {
+		XGoClass(f, cfg)
 		var buf bytes.Buffer
 		if err = format.Node(&buf, fset, f); err == nil {
 			ret = buf.Bytes()
@@ -101,8 +114,8 @@ func XGoClassSource(src []byte, pkg string, class string, proj bool, filename ..
 }
 
 // XGoClass format ast.File to Go+ class
-func XGoClass(file *ast.File, pkg string, class string, prog bool) {
-	formatClass(file, pkg, class, prog)
+func XGoClass(file *ast.File, cfg *ClassConfig) {
+	formatClass(file, cfg)
 }
 
 func findFuncDecl(decls []ast.Decl, name string) (int, *ast.FuncDecl) {
@@ -178,12 +191,11 @@ type importCtx struct {
 }
 
 type formatCtx struct {
-	imports   map[string]*importCtx
-	scope     *types.Scope
-	classMode bool   //class mode
-	classPkg  string //class pkg name
-	className string //this class
-	funcRecv  string //this class func recv
+	imports  map[string]*importCtx
+	scope    *types.Scope
+	classCfg *ClassConfig
+	classPkg string //this class pkg name
+	funcRecv string //this class func recv
 }
 
 func (ctx *formatCtx) insert(name string) {
@@ -249,14 +261,13 @@ func formatFile(file *ast.File) {
 	}
 }
 
-func formatClass(file *ast.File, pkg string, class string, proj bool) {
+func formatClass(file *ast.File, cfg *ClassConfig) {
 	var funcs []*ast.FuncDecl
 	ctx := &formatCtx{
-		imports:   make(map[string]*importCtx),
-		scope:     types.NewScope(nil, token.NoPos, token.NoPos, ""),
-		classMode: true,
-		classPkg:  path.Base(pkg),
-		className: class,
+		imports:  make(map[string]*importCtx),
+		scope:    types.NewScope(nil, token.NoPos, token.NoPos, ""),
+		classCfg: cfg,
+		classPkg: path.Base(cfg.PkgPath),
 	}
 	if file.Name.Name == "main" {
 		file.NoPkgDecl = true
@@ -268,17 +279,17 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 	for _, decl := range file.Decls {
 		switch v := decl.(type) {
 		case *ast.FuncDecl:
-			if isClassFunc(v, class) {
+			if isClassFunc(v, cfg.ClassName) {
 				v.IsClass = true
 				switch v.Name.Name {
 				case "MainEntry":
-					if proj {
+					if cfg.Project {
 						fnEntry = v
 						file.ShadowEntry = v
 						continue
 					}
 				case "Main":
-					if !proj {
+					if !cfg.Project {
 						fnEntry = v
 						file.ShadowEntry = v
 						continue
@@ -287,7 +298,7 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 				case "Classfname":
 					v.Shadow = true
 				}
-			} else if v.Name.Name == "main" && proj {
+			} else if v.Name.Name == "main" && cfg.Project {
 				v.Shadow = true
 			}
 		case *ast.GenDecl:
@@ -296,10 +307,10 @@ func formatClass(file *ast.File, pkg string, class string, proj bool) {
 				imports = append(imports, v)
 				continue
 			case token.TYPE:
-				if spec, ok := v.Specs[0].(*ast.TypeSpec); ok && spec.Name.Name == class {
+				if spec, ok := v.Specs[0].(*ast.TypeSpec); ok && spec.Name.Name == cfg.ClassName {
 					if st, ok := spec.Type.(*ast.StructType); ok {
 						for _, fs := range st.Fields.List {
-							if len(fs.Names) == 0 && pkg != "" {
+							if len(fs.Names) == 0 && cfg.PkgPath != "" {
 								continue
 							}
 							varSpecs = append(varSpecs, &ast.ValueSpec{Names: fs.Names, Type: fs.Type})
@@ -403,7 +414,7 @@ func funcRecv(v *ast.FuncDecl) *ast.Ident {
 }
 
 func formatFuncDecl(ctx *formatCtx, v *ast.FuncDecl) {
-	if ctx.classMode && isClassFunc(v, ctx.className) {
+	if ctx.classCfg != nil && isClassFunc(v, ctx.classCfg.ClassName) {
 		v.IsClass = true
 		if recv := funcRecv(v); recv != nil {
 			ctx.funcRecv = recv.Name